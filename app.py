0cf594cbcf6d03ebaf0074bc4dfe8c1539f0951f
phrubos <30824092+phrubos@users.noreply.github.com>
2022-09-03 13:49:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ profile_pic = Image.open(profile_pic)
 
 
 # --- HERO SECTION ----
-col1, col2 = st.columns((1,2), , gap="small")
+col1, col2 = st.columns((1,2) , gap="small")
 with col1:
     st.image(profile_pic, width=230)
 
