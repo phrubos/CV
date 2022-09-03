85e0694e9c9e1a05137e08ceb1003aba1922b28a
phrubos <30824092+phrubos@users.noreply.github.com>
2022-09-03 13:43:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -40,13 +40,6 @@ profile_pic = Image.open(profile_pic)
 col1, col2 = st.columns(2, gap="small")
 with col1:
     st.image(profile_pic, width=230)
-
-with col2:
-    st.title(NAME)
-    st.write("CE Productivity Process Manager")
-    st.write("---")
-    st.write(DESCRIPTION1)
-    st.write(DESCRIPTION2)
     st.download_button(
         label=" 📋 Download Resume",
         data=PDFbyte,
@@ -54,6 +47,15 @@ with col2:
         mime = "application/octet-stream",
     )
     st.write(" 📧 ", EMAIL)
+
+
+with col2:
+    st.title(NAME)
+    st.write("CE Productivity Process Manager")
+    st.write("---")
+    st.write(DESCRIPTION1)
+    st.write(DESCRIPTION2)
+
 
 # --- SOCIAL LINKS ---
 
