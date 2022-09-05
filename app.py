1b527bb1ef26e1206225fe2e5b1f2d3af001c854
phrubos <30824092+phrubos@users.noreply.github.com>
2022-09-05 16:16:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ with open(resume_file, "rb") as pdf_file:
 profile_pic = Image.open(profile_pic)
 
 
-# --- HERO SECTION ----
+# --- PROFIL PIC AND DESCRIPTIONS SECTION ----
 col1, col2 = st.columns((1,1) , gap="small")
 with col1:
     st.image(profile_pic, width=230)
