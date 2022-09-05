c30f4cd478c0f6e70d12400426161a1010e957bf
phrubos <30824092+phrubos@users.noreply.github.com>
2022-09-05 13:03:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ st.write("---")
 # --- SOCIAL LINKS ---
 
 st.write("#")
-cols = st.columns((2.2,4,1.65,3.7,1.55,3.8))
+cols = st.columns((2.2,4,1.65,4.1,1.55,3.8))
 # col1 = st.columns(1)
 # with col1:
 #             st.image(FB, width=25)
